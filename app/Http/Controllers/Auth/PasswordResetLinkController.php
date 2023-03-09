f8e6819d4698d12decd3a59b38c4b54a9e9a7722
Rishi Kumar <rk3141508gmail.com>
2023-03-10 02:14:26 +0530

    password reset

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Validation\ValidationException;
 
@@ -21,21 +25,36 @@ class PasswordResetLinkController extends Controller
     {
         $request->validate([
             'email' => ['required', 'email'],
+            'mpin' => ['required', 'digits:4']
         ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
+        // // We will send the password reset link to this user. Once we have attempted
+        // // to send the link, we will examine the response then see the message we
+        // // need to show to the user. Finally, we'll send out a proper response.
+        // $status = Password::sendResetLink(
+        //     $request->only('email')
+        // );
 
-        if ($status != Password::RESET_LINK_SENT) {
-            throw ValidationException::withMessages([
-                'email' => [__($status)],
-            ]);
-        }
+        // if ($status != Password::RESET_LINK_SENT) {
+        //     throw ValidationException::withMessages([
+        //         'email' => [__($status)],
+        //     ]);
+        // }
+        $user = User::where('email', $request['email'])->first();
+            $password = Str::random(8);
+            if (!$user || !Hash::check($request['mpin'], $user->mpin)) {
+                throw ValidationException::withMessages([
+                    'error' => 'Email and MPIN did not match'
+                ]);
+            }
+            Mail::raw("Dear User, Your new password for Login to Pesa24 is $password", function($message) use ($request){
+                $message->from('info@pesa24.co.in', 'Pesa24');
+                $message->to($request['email'], 'User');
+                $message->subject('Password reset');
+                $message->priority(1);
+            });
 
-        return response()->json(['status' => __($status)]);
+
+        return response()->json(['status' => 'Check your email for password']);
     }
 }
