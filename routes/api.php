82fd3cc713552272fd57a7c091a34fad36a13ebb
Rishi Kumar <rk3141508gmail.com>
2023-03-12 18:39:34 +0530

    method

--- routes/api.php
+++ routes/api.php
@@ -97,7 +97,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
 Route::group(['middleware' => ['auth:sanctum', 'role:admin'], 'prefix' => 'admin'], function () {
     Route::get('users', [UserController::class, 'index']);
     Route::get('users/{id}', [UserController::class, 'show']);
-    Route::post('create/user', [UserController::class, 'admin']);
+    Route::post('create/user', [UserController::class, 'store']);
     
 
 
