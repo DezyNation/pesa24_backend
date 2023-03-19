11def95c12a285051553936b9fe5c8e7988a3256
Rishi Kumar <rk3141508gmail.com>
2023-03-19 19:45:48 +0530

    service-aeps

--- app/Http/Controllers/Pesa24/AttachServiceController.php
+++ app/Http/Controllers/Pesa24/AttachServiceController.php
@@ -34,12 +34,12 @@ class AttachServiceController extends Controller
         return $data;
     }
 
-    public function attachService(Request $request, $id)
+    public function attachService($id)
     {
         $user = User::findOrfail(auth()->user()->id);
         $service = Service::findOrFail($id);
         if ($id == 45) {
-            $this->aepsEnroll($request);
+            $this->aepsEnroll(45);
             $this->paysprintOnboard();
         } else {
             $this->generalService($id);
@@ -47,29 +47,29 @@ class AttachServiceController extends Controller
         return response("Sucecssfully enrolled", 200);
     }
 
-    public function aepsEnroll(Request $request)
+    public function aepsEnroll($service_code)
     {
-        $key = "f74c50a1-f705-4634-9cda-30a477df91b7";
+        $key = env('EKO_KEY');
         $encodedKey = base64_encode($key);
         $secret_key_timestamp = round(microtime(true) * 1000);
         $signature = hash_hmac('SHA256', $secret_key_timestamp, $encodedKey, true);
         $secret_key = base64_encode($signature);
 
         $data = [
-            'service_code' => $request['serviceCode'],
-            'initiator_id' => '9962981729',
+            'service_code' =>   $service_code,
+            'initiator_id' => env('EKO_INITIATOR_ID'),
             'user_code' => auth()->user()->user_code,
             'modelname' => auth()->user()->model_name,
             'devicenumber' => auth()->user()->device_number,
-            'office_address' => json_encode(['line' => strval($request['line']), 'city' => strval($request['city']), 'state' => strval($request['state']), 'pincode' => strval($request['pincode'])]),
-            'address_as_per_proof' => json_encode(['line' => strval($request['lineProof']), 'city' => strval($request['cityProof']), 'state' => strval($request['stateProof']), 'pincode' => strval($request['pincodeProof'])])
+            'office_address' => json_encode(['line' => strval(auth()->user()->line), 'city' => strval(auth()->user()->city), 'state' => strval(auth()->user()->state), 'pincode' => strval(auth()->user()->pincode)]),
+            'address_as_per_proof' => json_encode(['line' => strval(auth()->user()->line), 'city' => strval(auth()->user()->city), 'state' => strval(auth()->user()->state), 'pincode' => strval(auth()->user()->pincode)])
         ];
         $pan = Storage::disk('local')->get(auth()->user()->pan);
         $aadhar_front = Storage::disk('local')->get(auth()->user()->aadhar_front);
         $aadhar_back = Storage::disk('local')->get(auth()->user()->aadhar_back);
 
         $response = Http::asForm()->attach('pancard', file_get_contents($pan), 'pan.pdf')->attach('aadhar_front', file_get_contents($aadhar_front), 'aadhar_front.pdf')->attach('aadhar_back', file_get_contents($aadhar_back), 'aadhar_back.pdf')->withHeaders([
-            'developer_key' => 'becbbce45f79c6f5109f848acd540567',
+            'developer_key' => env('EKO_DEVELOPER_KEY'),
             'secret-key-timestamp' => $secret_key_timestamp,
             'secret-key' => $secret_key,
         ])->put('https://staging.eko.in:25004/ekoapi/v1/user/service/activate', $data);
@@ -102,7 +102,7 @@ class AttachServiceController extends Controller
 
     public function generalService($id)
     {
-        $key = "f74c50a1-f705-4634-9cda-30a477df91b7";
+        $key = env('EKO_KEY');
         $encodedKey = base64_encode($key);
         $secret_key_timestamp = round(microtime(true) * 1000);
         $signature = hash_hmac('SHA256', $secret_key_timestamp, $encodedKey, true);
@@ -110,11 +110,11 @@ class AttachServiceController extends Controller
 
         $data = [
             'service_code' => $id,
-            'initiator_id' => '9962981729',
+            'initiator_id' => env('EKO_INITIATOR_ID'),
             'user_code' => auth()->user()->user_code,
         ];
         $response = Http::asForm()->withHeaders([
-            'developer_key' => 'becbbce45f79c6f5109f848acd540567',
+            'developer_key' => env('EKO_DEVELOPER_KEY'),
             'secret-key-timestamp' => $secret_key_timestamp,
             'secret-key' => $secret_key,
         ])->put('https://staging.eko.in:25004/ekoapi/v1/user/service/activate', $data);
