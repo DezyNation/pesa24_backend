8354de6c1e15abad3fdd7588628c11bb2ee9e302
DezyNation <dezynationindia@gmail.com>
2023-04-02 19:58:28 +0530

    dmt comm

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -163,7 +163,7 @@ class AdminController extends Controller
 
             case 'dmt':
                 $data = DB::table('d_m_t_s')->where('id', $request['id'])
-                    ->updateOrInsert(['from' => $request['from'], 'to' => $request['to'], 'package_id' => $request['package_id']], $request->all());
+                    ->updateOrInsert(['from' => $request['from'], 'to' => $request['to']], $request->all());
                 break;
 
             case 'payout':
