6db08ff69b259db9fa227bd560f3fbb05246d791
Rishi Kumar <rk3141508@gmail.com>
2023-04-05 03:16:55 +0530

    user_add

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -86,4 +86,160 @@ class RegisteredUserController extends Controller
 
     //     return "Mail sent";
     // }
+
+    public function registerAdmin(Request $request)
+    {
+        $request->validate([
+            'firstName' => ['required', 'string', 'max:255'],
+            'lastName' => ['required', 'string', 'max:255'],
+            'userEmail' => ['required', 'string', 'email', 'max:255', Rule::unique('users', 'email')],
+            'userPhone' => ['required', 'digits:10', Rule::unique('users', 'phone_number')],
+            'alternatePhone' => ['required', 'digits:10', Rule::unique('users', 'alternate_number')],
+            'dob' => ['required', 'date'],
+            'gender' => ['required', 'string', 'max:255'],
+            'firmName' => ['required', 'string', 'max:255'],
+            'companyType' => ['required', 'string', 'max:255'],
+            'aadhaarNum' => ['required', 'digits:12', Rule::unique('users', 'aadhaar')],
+            'panNum' => ['required', 'max:10', 'regex:/^([A-Z]){5}([0-9]){4}([A-Z]){1}/', Rule::unique('users', 'pan_number')],
+            'gst' => ['string'],
+            'isActive' => ['required', 'boolean'],
+            'capAmount' => ['required', 'integer'],
+            'line' => ['required', 'string'],
+            'city' => ['required', 'string'],
+            'state' => ['required', 'string'],
+            'pincode' => ['required', 'integer'],
+        ]);
+
+        $aadhaar_front = $request->file('aadhaarFront')->store('aadhar_front');
+        $aadhaar_back = $request->file('aadhaarBack')->store('aadhar_back');
+        $pan_card = $request->file('pan')->store('pan');
+
+        $email = $request['userEmail'];
+        $name =  $request['firstName'] . " " . $request['lastName'];
+        $mpin = rand(1001, 9999);
+        $password = Str::random(8);
+
+        $user = User::create([
+            'first_name' => $request['firstName'],
+            'last_name' => $request['lastName'],
+            'name' => $name,
+            'email' => $email,
+            'phone_number' => $request['userPhone'],
+            'alternate_number' => $request['alternatePhone'],
+            'password' => Hash::make($password),
+            'mpin' => Hash::make($mpin),
+            'gender' => $request['gender'],
+            'dob' => $request['dob'],
+            'aadhaar' => $request['aadhaarNumber'],
+            'minimum_balance' => $request['capAmount'],
+            'gst_number' => $request['gst'],
+            'is_active' => $request['isActive'],
+            'line' => $request['line'],
+            'city' => $request['city'],
+            'pincode' => $request['pincode'],
+            'state' => $request['state'],
+            'pan_number' => $request['pan'],
+            'firm_name' => $request['companyName'],
+            'firm_type' => $request['companyName'],
+            'profile' => 1,
+            'aadhaar_front' => $aadhaar_front,
+            'aadhaar_back' => $aadhaar_back,
+            'pan_photo' => $pan_card,
+            'organization_id' => auth()->user()->organization_id
+        ])->assignRole('retailer');
+
+
+        event(new Registered($user));
+
+        Mail::raw("Hello Your one time password is $password and Mpin'-$mpin", function ($message) use ($email, $name) {
+            $message->from('info@pesa24.co.in', 'RPay');
+            $message->to($email, $name);
+            $message->subject('Welcome to Pesa24');
+            $message->priority(1);
+        });
+
+        return response()->noContent();
+    }
+
+    public function adminUpdate(Request $request)
+    {
+        
+        $request->validate([
+            'firstName' => ['required', 'string', 'max:255'],
+            'lastName' => ['required', 'string', 'max:255'],
+            'userEmail' => ['required', 'string', 'email', 'max:255', Rule::unique('users', 'email')->ignore($request['userId'])],
+            'userPhone' => ['required', 'digits:10', Rule::unique('users', 'phone_number')->ignore($request['userId'])],
+            'alternatePhone' => ['digits:10', Rule::unique('users', 'alternate_number')->ignore($request['userId'])],
+            'dob' => ['required', 'date'],
+            'gender' => ['required', 'string', 'max:255'],
+            'firmName' => ['string', 'max:255'],
+            'companyType' => ['string', 'max:255'],
+            'aadhaarNum' => ['required', 'digits:12', Rule::unique('users', 'aadhaar')->ignore($request['userId'])],
+            'panNum' => ['required', 'max:10', 'regex:/^([A-Z]){5}([0-9]){4}([A-Z]){1}/', Rule::unique('users', 'pan_number')->ignore($request['userId'])],
+            'gst' => ['string'],
+            'isActive' => ['required', 'boolean'],
+            'capAmount' => ['required', 'integer'],
+            'line' => ['required', 'string'],
+            'city' => ['required', 'string'],
+            'state' => ['required', 'string'],
+            'pincode' => ['required', 'integer'],
+        ]);
+        
+        $user = User::where('organization_id', auth()->user()->organization_id)->findOrFail($request['userId']);
+        if ($request->hasFile('aadhaarFront')) {
+            $aadhaar_front = $request->file('aadhaarFront')->store('aadhar_front');
+        } else {
+            $aadhaar_front = $user->aadhaar_front;
+        }
+
+        if ($request->hasFile('aadhaarBack')) {
+            $aadhaar_back = $request->file('aadhaarBack')->store('aadhar_back');
+        } else {
+            $aadhaar_back = $user->aadhaar_back;
+        }
+
+        if ($request->hasFile('pan')) {
+            $pan_card = $request->file('pan')->store('pan');
+        } else {
+            $pan_card = $user->pan_photo;
+        }
+
+        if ($request->hasFile('profilePic')) {
+            $profile = $request->file('profilePic')->store('profile_pic');
+        } else {
+            $profile = $user->profile_pic;
+        }
+
+        $email = $request['userEmail'];
+        $name =  $request['firstName'] . " " . $request['lastName'];
+
+        $user = User::where('id', $request['userId'])->update([
+            'first_name' => $request['firstName'],
+            'last_name' => $request['lastName'],
+            'name' => $name,
+            'email' => $email,
+            'phone_number' => $request['userPhone'],
+            'alternate_number' => $request['alternatePhone'],
+            'gender' => $request['gender'],
+            'dob' => $request['dob'],
+            'aadhaar' => $request['aadhaarNum'],
+            'minimum_balance' => $request['capAmount'],
+            'gst_number' => $request['gst'],
+            'is_active' => $request['isActive'],
+            'line' => $request['line'],
+            'city' => $request['city'],
+            'pincode' => $request['pincode'],
+            'state' => $request['state'],
+            'pan_number' => $request['panNum'],
+            'firm_name' => $request['firmName'],
+            'firm_type' => $request['companyType'],
+            'profile' => 1,
+            'profile_pic' => $profile,
+            'aadhaar_front' => $aadhaar_front,
+            'aadhaar_back' => $aadhaar_back,
+            'pan_photo' => $pan_card,
+        ]);
+
+        return response()->noContent();
+    }
 }
