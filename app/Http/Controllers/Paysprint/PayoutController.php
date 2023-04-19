5d592cf192bd7bd7d713cc23c8940f67c88e60a2
DezyNation <dezynationindia@gmail.com>
2023-04-19 19:05:54 +0530

    time

--- app/Http/Controllers/Paysprint/PayoutController.php
+++ app/Http/Controllers/Paysprint/PayoutController.php
@@ -201,7 +201,7 @@ class PayoutController extends CommissionController
             'status' => true,
             'event' => 'money-transfer',
             'transaction_id' => $transaction_id,
-            'created_at' => now(),
+            'created_at' => date("F j, Y, g:i a");,
             'amount' => $request['amount'],
             'from' => auth()->user()->name . " " . auth()->user()->phone_number
         ];
@@ -225,7 +225,7 @@ class PayoutController extends CommissionController
             'event' => 'money-transfer',
             'amount' => $request['amount'],
             'transaction_id' => $transaction_id,
-            'created_at' => now(),
+            'created_at' => date("F j, Y, g:i a");,
         ];
         $user = User::findOrFail(auth()->user()->id);
         $final_amount = $user->wallet - $request['amount'];
