09a1f5ff71955cf617656371d13fbf0e87f79e5d
Rishi Kumar <rk3141508@gmail.com>
2023-04-22 15:50:24 +0530

    admin profile

--- app/Http/Controllers/Auth/ProfileController.php
+++ app/Http/Controllers/Auth/ProfileController.php
@@ -85,6 +85,8 @@ class ProfileController extends AgentManagementController
             $aadhaar_back = $request->file('values.aadhaarBack')->store('aadhar_back');
         } elseif ($request->hasFile('values.panCard')) {
             $pan_card = $request->file('values.panCard')->store('pan');
+        } elseif ($request->hasFile('values.profilePic')) {
+            $profile = $request->file('values.profilePic')->store('profile');
         }
 
         User::where('id', auth()->user()->id)->update([
@@ -102,6 +104,7 @@ class ProfileController extends AgentManagementController
             'company_name' => $request['values']['firmName'],
             'firm_type' => $request['values']['companyType'],
             'profile' => 1,
+            'profile_pic' => $profile,
             'aadhar_front' => $aadhaar_front ?? auth()->user()->aadhar_front,
             'aadhar_back' => $aadhaar_back ?? auth()->user()->aadhar_back,
             'pan_photo' => $pan_card ?? auth()->user()->pan_photo,
@@ -262,20 +265,20 @@ class ProfileController extends AgentManagementController
         $request->validate([
             'values.firstName' => ['required', 'max:255'],
             'values.lastName' => ['required', 'string', 'max:255'],
-            'values.dob' => ['required', 'date', 'before_or_equal:-13 years'],
-            'values.aadhaar' => ['exists:users,aadhaar', 'digits:12', 'integer', Rule::unique('users', 'aadhaar')->ignore(auth()->user()->id)],
-            'values.line' => ['required', 'string', 'max:255'],
-            'values.city' => ['required', 'string', 'max:255'],
-            'values.pincode' => ['required', 'digits:6', 'integer'],
-            'values.state' => ['required', 'string', 'max:255'],
+            // 'values.dob' => ['required', 'date', 'before_or_equal:-13 years'],
+            'values.aadhaar' => [Rule::unique('users', 'aadhaar')->ignore(auth()->user()->id)],
+            // 'values.line' => ['required', 'string', 'max:255'],
+            // 'values.city' => ['required', 'string', 'max:255'],
+            // 'values.pincode' => ['required', 'digits:6', 'integer'],
+            // 'values.state' => ['required', 'string', 'max:255'],
             'values.phone' => ['required', Rule::unique('users', 'phone_number')->ignore(auth()->user()->id)],
-            'values.pan' => ['required', 'regex:/^([A-Z]){5}([0-9]){4}([A-Z]){1}/', Rule::unique('users', 'pan_number')->ignore(auth()->user()->id)],
+            'values.pan' => [Rule::unique('users', 'pan_number')->ignore(auth()->user()->id)],
             'values.firmName' => ['max:255'],
-            'values.aadhaarFront' => ['file', 'mimes:jpg,jpeg,png', 'max:2048'],
-            'values.aadhaarBack' => ['file', 'mimes:jpg,jpeg,png', 'max:2048'],
-            'values.panCard' => ['file', 'mimes:jpg,jpeg,png', 'max:2048'],
-            'values.deviceNumber' => ['required', 'string'],
-            'values.modelName' => ['required', 'string'],
+            // 'values.aadhaarFront' => ['file', 'mimes:jpg,jpeg,png', 'max:2048'],
+            // 'values.aadhaarBack' => ['file', 'mimes:jpg,jpeg,png', 'max:2048'],
+            // 'values.panCard' => ['file', 'mimes:jpg,jpeg,png', 'max:2048'],
+            // 'values.deviceNumber' => ['required', 'string'],
+            // 'values.modelName' => ['required', 'string'],
         ]);
 
         if ($request->hasFile('values.aadhaarFront')) {
@@ -284,23 +287,26 @@ class ProfileController extends AgentManagementController
             $aadhaar_back = $request->file('values.aadhaarBack')->store('aadhar_back');
         } elseif ($request->hasFile('values.panCard')) {
             $pan_card = $request->file('values.panCard')->store('pan');
+        } elseif ($request->hasFile('values.profilePic')) {
+            $profile = $request->file('values.profilePic')->store('profile');
         }
 
         User::where('id', auth()->user()->id)->update([
             'first_name' => $request['values']['firstName'],
             'last_name' => $request['values']['lastName'],
-            'dob' => $request['values']['dob'],
-            'aadhaar' => $request['values']['aadhaar'],
-            'line' => $request['values']['line'],
-            'city' => $request['values']['city'],
-            'pincode' => $request['values']['pincode'],
-            'gst_number' => $request['values']['gst'],
-            'state' => $request['values']['state'],
-            'phone_number' => $request['values']['phone'],
-            'pan_number' => $request['values']['pan'],
-            'company_name' => $request['values']['firmName'],
-            'firm_type' => $request['values']['companyType'],
+            'dob' => $request['values']['dob'] ?? auth()->user()->dob,
+            'aadhaar' => $request['values']['aadhaar'] ?? auth()->user()->aadhaar,
+            'line' => $request['values']['line'] ?? auth()->user()->line,
+            'city' => $request['values']['city'] ?? auth()->user()->city,
+            'pincode' => $request['values']['pincode'] ?? auth()->user()->pincode,
+            'gst_number' => $request['values']['gst'] ?? auth()->user()->gst_number,
+            'state' => $request['values']['state'] ?? auth()->user()->state,
+            'phone_number' => $request['values']['phone'] ?? auth()->user()->phone_number,
+            'pan_number' => $request['values']['pan'] ?? auth()->user()->pan_number,
+            'company_name' => $request['values']['firmName'] ?? auth()->user()->company_name,
+            'firm_type' => $request['values']['companyType'] ?? auth()->user()->firm_type,
             'profile' => 1,
+            'profile_pic' => $profile ?? auth()->user()->profile_pic,
             'aadhar_front' => $aadhaar_front ?? auth()->user()->aadhar_front,
             'aadhar_back' => $aadhaar_back ?? auth()->user()->aadhar_back,
             'pan_photo' => $pan_card ?? auth()->user()->pan_photo,
