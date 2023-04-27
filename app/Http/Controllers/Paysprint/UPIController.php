e364fbf6f6718dc0a92675fb65ab1974bfc94cc6
DezyNation <dezynationindia@gmail.com>
2023-04-27 20:30:08 +0530

    mac shift

---
+++ app/Http/Controllers/Paysprint/UPIController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Paysprint;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UPIController extends Controller
+{
+    public function createVpa()
+    {
+        
+    }
+}
