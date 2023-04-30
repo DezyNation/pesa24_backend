e857da3b546d7faaadbf3a39472587c329d6e2b5
Peace Garden <peacegarden@Peaces-MacBook-Air.local>
2023-04-30 15:44:05 +0530

    razorpay

--- app/Http/Controllers/Razorpay/PayoutController.php
+++ app/Http/Controllers/Razorpay/PayoutController.php
@@ -76,7 +76,7 @@ class PayoutController extends CommissionController
                 'amount' => $data['amount'] / 100,
                 'reference_id' => $data['reference_id'],
                 'to' => $request['bank_account']['name'] ?? null,
-                'status' => $transfer->status()
+                'r_status' => $transfer->status()
             ];
             return response(['Transaction sucessfull', 'metadata' => $metadata], 200);
         }
