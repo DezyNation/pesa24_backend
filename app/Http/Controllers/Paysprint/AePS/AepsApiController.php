76b24ce294f9fdd5d461b4eae601a33d04b9144d
Rishi Kumar <rk3141508@gmail.com>
2023-05-09 20:27:40 +0530

    phone

--- app/Http/Controllers/Paysprint/AePS/AepsApiController.php
+++ app/Http/Controllers/Paysprint/AePS/AepsApiController.php
@@ -167,7 +167,7 @@ class AepsApiController extends CommissionController
             $this->transaction(0, "AePS withdrawal for {$data['mobilenumber']}", 'aeps', auth()->user()->id, $walletAmt[0], $transaction_id, $walletAmt[0], json_encode($metadata));
         }
         // $this->aepsCommssion($data['amount'], auth()->user()->id);
-        return [$response->object(), 'metadata' => $metadata];
+        return [$response->body(), 'metadata' => $metadata];
     }
 
     public function bankList()
