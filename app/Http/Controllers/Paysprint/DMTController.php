ef69f93bb1e182d5853d2258fc6a40f7d497cdfa
Rishi Kumar <rk3141508@gmail.com>
2023-05-11 09:50:55 +0530

    service api

--- app/Http/Controllers/Paysprint/DMTController.php
+++ app/Http/Controllers/Paysprint/DMTController.php
@@ -138,7 +138,7 @@ class DMTController extends CommissionController
             'Token' => $token,
             'Authorisedkey' => env('AUTHORISED_KEY'),
             'content-type' => 'application/json',
-        ])->post('https://paysprint.in/service-api/api/v1/service/dmt/beneficiary/registerbeneficiary/fetchbeneficiary', $data);
+        ])->post('https://api.paysprint.in/api/v1/service/dmt/beneficiary/registerbeneficiary/fetchbeneficiary', $data);
 
         return $response;
     }
@@ -208,7 +208,7 @@ class DMTController extends CommissionController
             'Token' => $token,
             'Authorisedkey' => env('AUTHORISED_KEY'),
             'content-type' => 'application/json',
-        ])->post('https://paysprint.in/service-api/api/v1/service/dmt/transact/transact', $data);
+        ])->post('https://api.paysprint.in/api/v1/service/dmt/transact/transact', $data);
 
         if ($response->json($key = 'status') == true) {
             $metadata = [
