c05e0ed852a7c36c8ffeccfb71b4639a76515504
Rishi <rk3141508@gmail.com>
2023-05-21 18:37:53 +0530

    storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -70,7 +70,7 @@ return [
     */
 
     'links' => [
-        "https://srv846.hstgr.io:7443/5809cc506c73e2a3/files/public_html/api/storage/profile" => storage_path('app/profile'),
+        public_path('storage/profile') => storage_path('app/profile'),
     ],
 
 ];
