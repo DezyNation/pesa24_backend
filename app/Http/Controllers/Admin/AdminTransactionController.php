cef8696805cd969fa4d989a6d16fe1bec2d85a0c
Rishi Kumar <rk3141508@gmail.com>
2023-06-11 21:08:26 +0530

    date

--- app/Http/Controllers/Admin/AdminTransactionController.php
+++ app/Http/Controllers/Admin/AdminTransactionController.php
@@ -29,7 +29,7 @@ class AdminTransactionController extends Controller
             $data = DB::table('transactions')
                 ->join('users', 'users.id', '=', 'transactions.user_id')
                 ->join('users as admin', 'admin.id', '=', 'transactions.trigered_by')
-                ->whereBetween('transactions.created_by', [$from, $to])
+                ->whereBetween('transactions.created_at', [$from, $to])
                 ->where(['transactions.service_type' => $data])
                 ->select('users.name', 'transactions.*', 'admin.organization_id', 'admin.first_name as done_by', 'admin.phone_number as done_by_phone')
                 ->latest()
@@ -41,7 +41,7 @@ class AdminTransactionController extends Controller
             ->join('users', 'users.id', '=', 'transactions.user_id')
             ->join('users as admin', 'admin.id', '=', 'transactions.trigered_by')
             ->where(['transactions.service_type' => $data, 'admin.organization_id' => auth()->user()->organization_id])
-            ->whereBetween('transactions.created_by', [$from, $to])
+            ->whereBetween('transactions.created_at', [$from, $to])
             ->select('users.name', 'transactions.*', 'admin.organization_id', 'admin.first_name as done_by', 'admin.phone_number as done_by_phone')
             ->latest()
             ->paginate(20);
