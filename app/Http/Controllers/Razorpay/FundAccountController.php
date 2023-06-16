c05e532985fef8a8ab9db60851799681cb11549b
Rishi Kumar <rk3141508@gmail.com>
2023-06-16 17:42:53 +0530

    send entry effect added

--- app/Http/Controllers/Razorpay/FundAccountController.php
+++ app/Http/Controllers/Razorpay/FundAccountController.php
@@ -12,15 +12,15 @@ use Illuminate\Support\Facades\Log;
 
 class FundAccountController extends PayoutController
 {
-    
-    
+
+
     public function createFundAcc(Request $request, $service_id)
     {
-        
+
         if(!Hash::check($request['mpin'], auth()->user()->mpin)){
             return response('MPIN did not match', 400);
         }
-        
+
             $account_details = [
                 'name' => $request['beneficiaryName'],
                 'ifsc' => $request['ifsc'],
@@ -36,12 +36,12 @@ class FundAccountController extends PayoutController
 
         $response = Http::withBasicAuth('rzp_test_f76VR5UvDUksZJ', 'pCcVlr5pRFcBZxAH4xBqGY62')
             ->post('https://api.razorpay.com/v1/fund_accounts', $data);
-    
+
             if (isset($response['error'])) {
                 return response()->json(['message' => $response['error']['description']], 400);
             }
             Log::channel('response')->info($response);
         return $this->bankPayout($response, $request['amount'], $service_id);
     }
-    
+
 }
