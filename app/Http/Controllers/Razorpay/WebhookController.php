6e200d28bd67f14483dda38229f82bcbe91bd63d
Rishi Kumar <rk3141508@gmail.com>
2023-06-16 15:22:56 +0530

    janpay

--- app/Http/Controllers/Razorpay/WebhookController.php
+++ app/Http/Controllers/Razorpay/WebhookController.php
@@ -29,9 +29,6 @@ class WebhookController extends CommissionController
             $user = User::findOrFail($result[0]->user_id);
             $opening_balance = $user->wallet;
             $closing_balance = $opening_balance + $request['payload.payout.entity.amount'] / 100;
-            $user->update([
-                'wallet' => $closing_balance
-            ]);
             $transaction_id = "REV" . strtoupper(Str::random(5));
             $this->transaction(0, "Payout Reversal", 'payout', $result[0]->user_id, $opening_balance, $transaction_id, $closing_balance, $request['payload.payout.entity.amount'] / 100);
             $commission = $this->razorpayReversal($result[0]->amount / 100, $result[0]->user_id);
