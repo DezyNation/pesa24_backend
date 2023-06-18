d66e0ab4728b420be7bec0aaccb12dfa3e17257a
Rishi Kumar <rk3141508@gmail.com>
2023-06-18 21:59:02 +0530

    ref_id

--- app/Http/Controllers/Razorpay/PayoutController.php
+++ app/Http/Controllers/Razorpay/PayoutController.php
@@ -47,7 +47,7 @@ class PayoutController extends CommissionController
             'ifsc' => $account_details['ifsc'],
             'mode' => $transfer['mode'] ?? 0,
             'purpose' => $transfer['purpose'] ?? 0,
-            'reference_id' => $transfer['reference_id'] ?? 0,
+            'reference_id' => $data['reference_id'] ?? 0,
             'narration' => $transfer['narration'] ?? 0,
             'batch_id' => $transfer['batch_id'] ?? 0,
             'description' => $transfer['status_details']['description'] ?? 0,
