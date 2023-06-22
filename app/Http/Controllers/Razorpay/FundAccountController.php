77867a946936ac9c3c4dae271a5070df39f4d73d
Rishi Kumar <rk3141508@gmail.com>
2023-06-22 07:03:59 +0530

    webhook

--- app/Http/Controllers/Razorpay/FundAccountController.php
+++ app/Http/Controllers/Razorpay/FundAccountController.php
@@ -17,30 +17,31 @@ class FundAccountController extends PayoutController
     public function createFundAcc(Request $request)
     {
 
-        if (!Hash::check($request['mpin'], auth()->user()->mpin)) {
+        if(!Hash::check($request['mpin'], auth()->user()->mpin)){
             return response('MPIN did not match', 400);
         }
 
-        $account_details = [
-            'name' => $request['beneficiaryName'],
-            'ifsc' => $request['ifsc'],
-            'account_number' => $request['account']
-        ];
+            $account_details = [
+                'name' => $request['beneficiaryName'],
+                'ifsc' => $request['ifsc'],
+                'account_number' => $request['account']
+            ];
 
 
         $data = [
-            'contact_id' => 'cont_LLryH8Bm2cJaPz',
+            'contact_id' => auth()->user()->rzp_contact_id,
             'account_type' => 'bank_account',
             'bank_account' => $account_details
         ];
 
-        $response = Http::withBasicAuth('rzp_test_f76VR5UvDUksZJ', 'pCcVlr5pRFcBZxAH4xBqGY62')
+        $response = Http::withBasicAuth('rzp_live_XgWJpiVBPIl3AC', '1vrEAOIWxIxHkHUQdKrnSWlF')
             ->post('https://api.razorpay.com/v1/fund_accounts', $data);
 
-        if (isset($response['error'])) {
-            return response()->json(['message' => $response['error']['description']], 400);
-        }
-        Log::channel('response')->info($response);
+            Log::channel('response')->info($response);
+            if (isset($response['error'])) {
+                return response()->json(['message' => $response['error']['description']], 400);
+            }
         return $this->bankPayout($response, $request['amount'], $account_details);
     }
+
 }
