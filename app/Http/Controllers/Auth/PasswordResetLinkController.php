31d4264f7c98e7c90746f827ea9cc48c47e2b185
Rishi <rk3141508@gmail.com>
2023-06-22 21:52:20 +0530

    details

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -79,13 +79,14 @@ class PasswordResetLinkController extends Controller
             // 'credential_remarks' => $request['remarks']
         ]);
 
-        $phone = $user->get('phone_number');
-        $phone = $phone[0];
+        $phone = $user->get();
+        $phone = $phone->phone_number;
+        $name = $phone->name;
         // SMS api
 
-        $newmsg = "Dear $email, Welcome to Rpay. You have registered sucessfully, your ID'-$phone, Password'-$password, Mpin'-$mpin Now you can login https://rpay.live/. From'-P24 Technology Pvt. Ltd";
+        $newmsg = "Dear $name , You have registered sucessfully, your ID'-$phone, Password'-$password, Mpin'-$mpin Don't Share anyone. From'-P24 Technology Pvt. Ltd";
         $sms = Http::post("http://alerts.prioritysms.com/api/web2sms.php?workingkey=Ab6a47904876c763b307982047f84bb80&to=$phone&sender=PTECHP&message=$newmsg", []);
-        Log::channel('response')->info('sms-creds', $sms->json());
+        // Log::channel('response')->info('sms-creds', $sms->json());
         Mail::raw("Dear User, Your new password for Login to Janpay is $password and MPIN is $mpin", function ($message) use ($request) {
             $message->from('info@pesa24.co.in', 'Janpay');
             $message->to($request['email'], $request['name']);
