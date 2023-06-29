5939de37905af2732fbc55f0a91d91c5acb199e3
Rishi Kumar <rk3141508@gmail.com>
2023-06-29 23:47:27 +0530

    user

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -271,11 +271,17 @@ class UserController extends Controller
         return $user;
     }
 
-    public function userInfo(string $role, $id = null)
+    public function userInfo(Request $request, string $role, $id = null)
     {
+        $search = $request['search'];
         $org_id = auth()->user()->organization_id;
         if (is_null($id)) {
             $user = User::role($role)->with('packages:name')->where(['organization_id' => $org_id])->paginate(100);
+            return $user;
+        }
+
+        if (!empty($search)||!is_null($search)) {
+            $user = User::role($role)->with('packages:name')->where(['organization_id' => $org_id])->where('id', 'like', '%'.$search.'%')->orWhere('phone_number', 'like', '%'.$search.'%')->get();
             return $user;
         }
 
