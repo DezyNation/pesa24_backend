98f861132c25b4977b724b9fd9bf1d56969ad17f
Rishi <rk3141508@gmail.com>
2023-07-09 00:31:26 +0530

    admin

--- app/Http/Controllers/Pesa24/FundController.php
+++ app/Http/Controllers/Pesa24/FundController.php
@@ -55,7 +55,7 @@ class FundController extends Controller
             ->join('users as admin', 'admin.id', '=', 'funds.parent_id')
             ->where('transaction_type', 'transfer')->orWhere('transaction_type', 'reversal')
             ->whereBetween('funds.created_at', [$request['from'] ?? Carbon::today(), $request['to'] ?? Carbon::tomorrow()])
-            ->select('users.name', 'users.phone_number', 'funds.transaction_id', 'funds.user_id', 'funds.amount', 'funds.remarks', 'funds.transaction_type', 'funds.created_at', 'admin.name as admin_name', 'admin.id as admin_id', 'funds.id')
+            ->select('users.name', 'users.phone_number', 'funds.transaction_id', 'funds.user_id', 'funds.amount', 'funds.remarks', 'funds.transaction_type', 'funds.created_at', 'admin.name as admin_name', 'admin.id as admin_id', 'funds.id', 'admin.phone_number as admin_phone')
             ->latest('funds.updated_at')
             ->paginate(100);
         return $data;
