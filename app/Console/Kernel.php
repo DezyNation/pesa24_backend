58c8afa10c25b57576e454a567e08f08401692db
Rishi Kumar <rk3141508@gmail.com>
2023-07-15 02:21:32 +0530

    cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,18 @@
 
 namespace App\Console;
 
+use DateTimeZone;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected function scheduleTimezone(): DateTimeZone|string|null
+{
+    return 'Asia/Kolkata';
+}
     /**
      * Define the application's command schedule.
      *
@@ -15,6 +22,17 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->call(function(){
+            $wallet_sum = User::role(['retailer', 'distributor', 'super_distributor'])->sum('wallet');
+            $capped_sum = User::role(['retailer', 'distributor', 'super_distributor'])->sum('minimum_balance');
+            DB::table('market_balance')->insert([
+                'market_balance' => $wallet_sum,
+                'capped_balance' => $capped_sum,
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        })
+        ->dailyAt('02:35');
         // $schedule->command('inspire')->hourly();
     }
 
