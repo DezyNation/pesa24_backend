497b098a0d56017a39cdbbae45593e99edb114c8
Rishi <rk3141508@gmail.com>
2023-08-07 16:55:46 +0530

    dev

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -17,7 +17,7 @@ class TestController extends Controller
             ->join('users as beneficiaries', 'beneficiaries.id', '=', 'transactions.user_id')
             ->join('model_has_roles', 'model_has_roles.model_id', '=', 'users.id')
             ->join('roles', 'roles.id', '=', 'model_has_roles.role_id')
-            ->select('transactions.*', 'users.name as trigered_by_name', 'users.phone_number as trigered_by_phone', 'users.organization_id', 'beneficiaries.name', 'beneficiaries.phone_number', 'users.wallet as wallet_amount')
+            ->select('users.name as trigered_by_name', 'users.phone_number as trigered_by_phone', 'users.organization_id', 'beneficiaries.name', 'beneficiaries.phone_number', 'users.wallet as wallet_amount', 'transactions.user_id', 'transactions.trigered_by', 'transactions.credit_amount', 'transactions.debit_amount')
             ->where('users.organization_id', 7)
             ->where('roles.name', '!=', 'admin')
             ->whereBetween('transactions.created_at', [$request['from'] ?? Carbon::now()->startOfDecade(), $request['to'] ?? Carbon::tomorrow()])
