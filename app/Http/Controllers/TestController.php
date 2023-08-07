62ab04b85d35f5b9727e0ad44a3a3ea08e41c683
Rishi <rk3141508@gmail.com>
2023-08-07 15:57:41 +0530

    db

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,16 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
 
 class TestController extends Controller
 {
     public function test(Request $request)
     {
-        $cache = Cache::put($request['time'], true);
-        Log::channel('response')->info('request', $request->all());
-        return $cache;
+        $data = DB::table('transactions')
+        ->join('users', 'users.id', '=', 'transactions.trigered_by')
+        ->join('users as beneficiaries', 'beneficiaries.id', '=', 'transactions.user_id')
+        ->join('model_has_roles', 'model_has_roles.model_id', '=', 'users.id')
+        ->join('roles', 'roles.id', '=', 'model_has_roles.role_id')
+        ->select('transactions.*', 'users.name as trigered_by_name', 'users.phone_number as trigered_by_phone', 'users.organization_id', 'beneficiaries.name', 'beneficiaries.phone_number', 'users.wallet as wallet_amount')
+        ->where('users.organization_id', auth()->user()->organization_id)
+        ->where('roles.name', '!=', 'admin')
+        ->whereBetween('transactions.created_at', [$request['from'] ?? Carbon::today(), $request['to'] ?? Carbon::tomorrow()])
+        ->latest('transactions.created_at')
+        ->groupBy(['trigered_by', 'service_type'])
+        ->get();
+
+        return $data;
     }
 }
