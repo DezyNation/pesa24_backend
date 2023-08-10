219bc90c5cd8921b8ecbab3de544702bf58193b5
Rishi <rk3141508@gmail.com>
2023-08-10 16:15:59 +0530

    idem and min_bal

--- app/Http/Middleware/Idempotency.php
+++ app/Http/Middleware/Idempotency.php
@@ -17,7 +17,7 @@ class Idempotency
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Cache::has($request['time'])) {
+        if (Cache::has($request->header('x-razorpay-event-id'))) {
             Log::channel('reversal')->info('idempotency', $request->all());
             return response("Request is processing", 200);
         }
