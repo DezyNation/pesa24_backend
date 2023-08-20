0d4b0b4d21e791747ca515bd5b02e003caed1496
Rishi <rk3141508@gmail.com>
2023-08-21 00:33:59 +0530

    aeps authentication enabled

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -337,11 +337,12 @@ class UserController extends Controller
         if (is_null($id)) {
             $user = DB::table('user_parent')
                 ->join('users', 'users.id', '=', 'user_parent.user_id')
-                ->join('model_has_roles', 'model_has_roles.model_id', '=', 'users.id')
-                ->join('package_user', 'package_user.user_id', '=', 'users.id')
-                ->join('packages', 'packages.id', 'package_user.package_id')
-                ->join('roles', 'roles.id', '=', 'model_has_roles.role_id')
-                ->where(['user_parent.parent_id' => auth()->user()->id, 'roles.name' => $role])
+                // ->join('model_has_roles', 'model_has_roles.model_id', '=', 'users.id')
+                // ->join('package_user', 'package_user.user_id', '=', 'users.id')
+                // ->join('packages', 'packages.id', 'package_user.package_id')
+                // ->join('roles', 'roles.id', '=', 'model_has_roles.role_id')
+                ->where(['user_parent.parent_id' => auth()->user()->id])
+                // , 'roles.name' => $role])
                 ->select('users.id', 'users.name', 'users.email', 'users.phone_number', 'users.alternate_phone', 'users.line', 'users.line', 'users.city', 'users.state', 'users.pincode', 'users.wallet', 'users.minimum_balance', 'users.kyc', 'roles.name as role_name', 'users.aadhar_front', 'users.aadhar_back', 'users.pan_photo', 'users.profile_pic', 'packages.name as package_name', 'users.gender', 'users.dob', 'users.gst_number')
                 ->get();
             return  $user;
