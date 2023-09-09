e1538efc62468b1b78cbb7abd458d25a002976ad
Rishi <rk3141508@gmail.com>
2023-09-09 12:28:14 +0530

    wallt

--- app/Http/Controllers/Razorpay/WebhookController.php
+++ app/Http/Controllers/Razorpay/WebhookController.php
@@ -16,6 +16,7 @@ class WebhookController extends CommissionController
     public function confirmPayout(Request $request)
     {
         Log::channel('callback')->info('callback-razorpay', $request->all());
+        Cache::put($request->header('x-razorpay-event-id'), $request->header('x-razorpay-event-id'), 600);
         if ($request['payload']['payout']['entity']['created_at'] - $request['created_at'] < 15 || $request['payload']['payout']['entity']['created_at'] - $request['created_at'] > 300) {
             Log::channel('reversals')->info('timings', [
                 'payout_timing' => $request['payload']['payout']['entity']['created_at'],
@@ -23,7 +24,6 @@ class WebhookController extends CommissionController
             ]);
             return true;
         }
-        Cache::put($request->header('x-razorpay-event-id'), $request->header('x-razorpay-event-id'), 600);
         Cache::put($request['payload.payout.entity.id'], $request['payload.payout.entity.id'], 300);
         DB::transaction(function () use ($request) {
 
