d3bed2e3b15ea0a811290494248131b16599988a
Rishi Kumar <rk3141508@gmail.com>
2023-09-10 16:47:56 +0530

    cache

--- app/Http/Controllers/Razorpay/PayoutController.php
+++ app/Http/Controllers/Razorpay/PayoutController.php
@@ -34,7 +34,7 @@ class PayoutController extends CommissionController
             ],
             'reference_id' => "JND" . uniqid(),
         ];
-        Cache::put(time() . auth()->user()->id, time() . auth()->user()->id, 3);
+        // Cache::put(time() . auth()->user()->id, time() . auth()->user()->id, 3);
         $transfer =  Http::withBasicAuth('rzp_live_XgWJpiVBPIl3AC', '1vrEAOIWxIxHkHUQdKrnSWlF')->withHeaders([
             'Content-Type' => 'application/json'
         ])->post('https://api.razorpay.com/v1/payouts', $data);
