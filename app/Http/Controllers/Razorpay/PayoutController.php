42610a98701b1e85b561eaf2d3288f9d43d86675
Rishi Kumar <rk3141508@gmail.com>
2023-09-10 16:49:20 +0530

    cache

--- app/Http/Controllers/Razorpay/PayoutController.php
+++ app/Http/Controllers/Razorpay/PayoutController.php
@@ -34,7 +34,7 @@ class PayoutController extends CommissionController
             ],
             'reference_id' => "JND" . uniqid(),
         ];
-        // Cache::put(time() . auth()->user()->id, time() . auth()->user()->id, 3);
+        Cache::put(time() . auth()->user()->id, time() . auth()->user()->id, 3);
         $transfer =  Http::withBasicAuth('rzp_live_XgWJpiVBPIl3AC', '1vrEAOIWxIxHkHUQdKrnSWlF')->withHeaders([
             'Content-Type' => 'application/json'
         ])->post('https://api.razorpay.com/v1/payouts', $data);
@@ -262,7 +262,7 @@ class PayoutController extends CommissionController
                 return response($payout->status);
             }
             // Cache::put(time(), time());
-            // Cache::put(time() . $payout->user_id, time() . $payout->user_id, 3);
+            Cache::put(time() . $payout->user_id, time() . $payout->user_id, 3);
             $transfer =  Http::withBasicAuth('rzp_live_XgWJpiVBPIl3AC', '1vrEAOIWxIxHkHUQdKrnSWlF')->withHeaders([
                 'Content-Type' => 'application/json'
             ])->get("https://api.razorpay.com/v1/payouts/$id");
@@ -316,5 +316,7 @@ class PayoutController extends CommissionController
                 ]
             ];
         });
+
+        return $res;
     }
 }
